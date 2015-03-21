74966d5b24bb779260aaac92bc438e49ef3595a0
adyall <adyall@gmail.com>
2015-03-21 22:52:28 +0400

    first attempt - prog assign 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix: usage mx <- makeCacheMatrix(m) 
+## where (m) is a matrix
+## A list (mx) is created containing the following functions:
+##      mx$getmat(): gets the matrix (m)
+##      mx$setmat(new_m): sets the matrix (new_m)
+##      mx$getinvmat(): retrieves the inverse of the matrix stored in cache
+##      mx$setinvmat(invmat): stores the inverse of the matrix to cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+    ## Initialize variables     
+    invmat <- NULL
+    
+    
+    setmat <- function(y) { ## sets the matrix
+      x <<- y
+      invmat <<- NULL
+    } 
 
+    getmat <- function() x  ## gets the matrix
+    
+    getinvmat <- function() invmat  ## retrieves the inverse of the matrix
+    setinvmat <- function(cache) invmat <<- cache ##stores the inverse to cache
+    
+    list(setmat = setmat, getmat = getmat, 
+         setinvmat = setinvmat,
+         getinvmat = getinvmat)  ## returns list
 }
 
 
-## Write a short comment describing this function
+
+## CacheSolve: usage CacheSolve(mx)
+## This function computes the inverse of the special "matrix" (x)
+## returned by makeCacheMatrix above. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+    ## retrieve the inverse from the cache
+    invmat <- x$getinvmat()
+    
+    
+    ## If the inverse has already been calculated, 
+    ## then return the results from cache.
+    
+    if(!is.null(invmat)) {
+      message("getting cached data")
+      return(invmat)
+    }
+    
+    ## else compute the inverse of the matrix and set result to cache    
+    data <- x$getmat()
+    invmat <- solve(data, ...)
+    x$setinvmat(invmat)
+    invmat    
 }
